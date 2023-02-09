cc9802c5fa324f2dc9225f194e0471da8da5bb08
CRISTIAN <CRISTIAN@DESKTOP-2VQND5A>
2023-02-09 19:16:15 +0100

    sadg

---
+++ src/tarea/Main.java
@@ -0,0 +1,24 @@
+
+package tarea;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author CRISTIAN
+ */
+public class Main
+extends JFrame {
+    public Main() {
+        JLabel lblSaludo = new JLabel("Hola Mundo. Creando mi primer ejemplo");
+        add(lblSaludo);
+        this.setSize(400,200);
+        this.setTitle("JFrame");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+    }
+}
